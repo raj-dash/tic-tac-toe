80300d6d61c4553f4ed7c96d90e8aab18aa0aaf6
Raj Aryan Dash <rajaryanrad@gmail.com>
2022-08-23 22:16:12 +0530

    setup board and a function to draw it

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8','9'}};
+
+void drawBoard() {
+    cout << "   |   |   " << endl;
+    cout << ' ' << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+    cout << "___|___|___" << endl;
+    cout << "   |   |   " << endl;
+    cout << ' ' << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+    cout << "___|___|___" << endl;
+    cout << "   |   |   " << endl;
+    cout << ' ' << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+    cout << "   |   |   " << endl;
+}
+
 int main() {
-    return 0;
+    drawBoard();
 }
