939413c94034f3bf905f1328ec366cd1c8718861
Raj Aryan Dash <rajaryanrad@gmail.com>
2022-08-24 13:06:36 +0530

    Add the following changes:     1. Function to swap players and the markers as needed, name is quite obvious     2. Function with the actual game loop and logic called "game()"     3. Called in all functions either within game() or main()

--- main.cpp
+++ main.cpp
@@ -54,18 +54,63 @@ char checkWin() {
         return current_player;
     }
 
-    return '0';
+    return 0;
+}
+
+// function to swap the player and the marker
+void swap_player_and_marker() {
+    if (current_marker == 'X') {
+        current_marker = 'O';
+    } else {
+        current_marker = 'X';
+    }
+
+    if (current_player == 1) {
+        current_player = 2;
+    } else {
+        current_player = 1;
+    }
+}
+
+// function with the actual logic of the game
+void game() {
+    cout << "Player 1, choose your marker (X or O) :" << endl;
+    char marker_p1;
+    cin >> marker_p1;
+    int winner;
+
+    current_player = 1;
+    current_marker = marker_p1;
+
+    drawBoard();
+
+    for (int i = 0; i < 9; i++) {
+        cout << "it's player " << current_player << "'s turn, enter your slot :" << endl;
+        int slot;
+        cin >> slot;
+
+        placeMarker(slot);
+        drawBoard();
+
+        winner = checkWin();
+        if (winner == 1) {
+            cout << endl << endl;
+            cout << "Congratulations! PLayer 1 has won!" << endl;
+            break;
+        }
+        if (winner == 2) {
+            cout << endl << endl;
+            cout << "Congratulations! PLayer 2 has won!" << endl;
+            break;
+        }
+
+        swap_player_and_marker();
+    }
+    if (winner == 0) {
+        cout << "Draw!" << endl;
+    }
 }
 
 int main() {
-    drawBoard();
-    current_marker = 'X';
-    current_player = '1';
-    placeMarker(1);
-    placeMarker(5);
-    placeMarker(9);
-    cout << endl << endl << endl;
-    drawBoard();
-
-    cout << checkWin();
+    game();
 }
